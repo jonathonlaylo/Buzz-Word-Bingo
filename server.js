93f8892f109692cf8eb99f9d886189e5154c490d
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-11 18:03:43 -1000

    current sever.js

--- server.js
+++ server.js
@@ -2,10 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const querystring = require('querystring');
+const app = express();
 
-var app = express();
 var buzzwordArray= [];
-var points = 0;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static('./public'));
@@ -15,25 +14,45 @@ app.get('/', function(req, res){
 });
 
 app.get('/buzzwords', function(req, res, next){
-  res.json({"buzzword": buzzwordArray});
+  res.json({'buzzword': buzzwordArray});
 });
 
-app.post('/buzzword', function(req, res){
-  //testing req.body
+app.post('/buzzword', function(req, res, next){
+  buzzwordArray.push(req.body);
+  res.json({'success': 'true'});
   console.log(req.body);
-
+  console.log(req.body.buzzword);
+  console.log(req.body.points);
 });
 
 app.put('/buzzword', function(req, res, next){
-
+  for (var i = 0; i < buzzwordArray.length; i++) {
+    if (req.body.buzzword === buzzwordArray[i].buzzword){
+      let buzzwordArrayPoints = parseInt(buzzwordArray[i].points);
+      let buzzwordReqPoints = parseInt(req.body.points);
+      buzzwordArray[i].heard = true;
+      console.log(buzzwordArray[i].heard);
+      buzzwordArray[i].points = buzzwordArrayPoints + buzzwordReqPoints;
+      console.log(buzzwordArray[i].points);
+      res.json({'success': true, 'newScore': buzzwordArray[i].points});
+    } else {
+      res.json({'success': false});
+    }
+  }
 });
 
 app.delete('/buzzword', function(req, res, next){
-
+  for (var i = 0; i < buzzwordArray.length; i++) {
+    if (req.body.buzzword === buzzwordArray[i].buzzword){
+      buzzwordArray.splice(i, 1, 0);
+      res.json({'success': true});
+    }
+  }
 });
 
-app.post('/rest', function(req, res){
-
+app.post('/reset', function(req, res, next){
+  buzzwordArray = [];
+  res.json({'success': true});
 });
 
 var server = app.listen(3000, function () {
